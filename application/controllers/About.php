c640b165fa320021d2c1e98cb77db24941b68c01
jtekenos <jessicatekenos@gmail.com>
2015-09-17 20:43:52 -0700

    initial commit

---
+++ application/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class About extends CI_Controller {
+    public function index()
+	{
+		$this->load->view('about');
+	}
+}
